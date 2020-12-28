265626fc02313b9614f0ae9ab18dc12f53c97751
Valdo <vlorian22@GMAIL.COM>
2020-12-27 21:03:30 -0600

    added switch case to carry the choices to to the next function

--- server.js
+++ server.js
@@ -1,33 +1,63 @@
-const inquirer =require ('inquirer');
-const sql= require('mysql2');
-const cnsTble= require ('console.table');
+const inquirer = require("inquirer");
+const sql = require("mysql2");
+const cnsTble = require("console.table");
 //const db= require('.');
 
+// create the connection to database
 
-// create the connection to database 
+const connection = sql.createConnection({
+  host: "localhost",
+  port: 3306,
 
-const connection= sql.createConnection({
-  host: 'localhost',
-  port:3306,
-
-  user:'root',
-  password:'root',
-  database: 'emp_tracker_db'
+  user: "root",
+  password: "root",
+  database: "emp_tracker_db",
 });
 
-connection.connect(err=>{
-  if(err) throw err;
-  console.log('connected as id' + connection.threadId);
- afterConnection();
+connection.connect((err) => {
+  if (err) throw err;
+  console.log("connected as id" + connection.threadId);
+  afterConnection();
 });
 
-afterConnection= ()=>{
-    inquirer.prompt([{
-            type:'list',
-            name: 'selection',
-            message: 'Select from the options',
-            choices:['View All Departments','View all Roles', 'View all Employees','Update Employees','ADD Department','ADD ROLE','ADD EMPLOYEE']
-    }]);
- 
-     connection.end();
- };
+afterConnection = () => {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "selection",
+        message: "Select from the options",
+        choices: [
+          "View All Departments",
+          "View all Roles",
+          "View all Employees",
+          "Update Employees",
+          "ADD Department",
+          "ADD ROLE",
+          "ADD EMPLOYEE",
+        ],
+      },
+    ])
+    .then((selections) => {
+      switch (selections.selection) {
+        case "View All Departments":
+          break;
+        case "View All Roles":
+          break;
+        case "View All Employees":
+          break;
+        case "Update Employees":
+          break;
+        case "ADD Department":
+          break;
+        case "ADD ROLE":
+          break;
+        case "ADD EMPLOYEE":
+          break;
+        default:
+          break;
+      }
+    });
+
+  connection.end();
+};
