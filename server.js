8ae0c67b9d9a68c9c7d0487019fa5dc3693eed67
Valdo <vlorian22@GMAIL.COM>
2020-12-29 09:14:32 -0600

    working on roles

--- server.js
+++ server.js
@@ -39,99 +39,99 @@ afterConnection = () => {
       },
     ])
     .then((selections) => {
-        
       switch (selections.selection) {
-          
         case "View All Departments":
-         connection.query('SELECT *FROM department',function(err,result){
-            if(err) {
-                console.log(err,'Nothing red');
+          connection.query("SELECT *FROM department", function (err, result) {
+            if (err) {
+              console.log(err, "Nothing red");
             }
             console.table(result);
-         });
+          });
           break;
         case "View All Roles":
           break;
         case "View all Employees":
-            connection.query('SELECT *FROM employee',function(err,result){
-                if(err) {
-                    console.log(err,'Nothing red');
-                }
-                console.table(result);
-             });
+          connection.query("SELECT *FROM employee", function (err, result) {
+            if (err) {
+              console.log(err, "Nothing red");
+            }
+            console.table(result);
+          });
           break;
         case "Update Employees":
           break;
         case "ADD Department":
-            inquirer.prompt([{
-                type: 'input',
-                name: 'department',
-                message: 'The name of the department your adding?'
-      }]).then(addD=>{
-          
-           
-          connection.query(`INSERT INTO department(name)VALUE('${addD.department}')`,function(err,result){
-             if(err) console.log( err);
-              console.table(result);
-          });
-        });
-        
+          inquirer
+            .prompt([
+              {
+                type: "input",
+                name: "department",
+                message: "The name of the department your adding?",
+              },
+            ])
+            .then((addD) => {
+              connection.query(
+                `INSERT INTO department(name)VALUE('${addD.department}')`,
+                function (err, result) {
+                  if (err) console.log(err);
+                  console.table(result);
+                }
+              );
+            });
+
           break;
         case "ADD ROLE":
-            
           break;
         case "ADD EMPLOYEE":
-              
-            inquirer.prompt([{
-                type: 'input',
-                name: 'first',
-                message: 'The first name of the employee?'
-      }, 
-      {
-          type: 'input',
-          name: 'last',
-          message: 'What is the last name of the employee?'
-      },
-      {
-          type:'input',
-          name: 'roleId',
-          message: 'What is the employees role id number?'
-      }, 
-        {
-            type: 'input',
-            name: 'managerId',
-            message: 'what is the employees managers id?'
-        }
+          inquirer
+            .prompt([
+              {
+                type: "input",
+                name: "first",
+                message: "The first name of the employee?",
+              },
+              {
+                type: "input",
+                name: "last",
+                message: "What is the last name of the employee?",
+              },
+              {
+                type: "input",
+                name: "roleId",
+                message: "What is the employees role id number?",
+              },
+              {
+                type: "input",
+                name: "managerId",
+                message: "what is the employees managers id?",
+              },
+            ])
+            .then((emP) => {
+              connection.query(
+                `INSERT INTO employee(first_name,last_name,role_id,manager_id)VALUE('${emP.first}','${emP.last}',${emP.roleId},${emP.managerId})`,
+                function (err, result) {
+                  if (err) console.log(err);
+                  console.log(result);
+                }
+              );
+            });
 
-    ]).then(emP=>{
-          
-           
-          connection.query(`INSERT INTO employee(first_name,last_name,role_id,manager_id)VALUE('${emP.first}','${emP.last}',${emP.roleId},${emP.managerId})`,function(err,result){
-             if(err) console.log( err);
-              console.log(result);
-          });
-        });
-         
-    
           break;
         default:
-            connection.end();
+          connection.end();
           break;
       }
     });
 
- // 
-//};
-// function viewAllDepartments(){
-   
-//   return connection.query(sqlQuery,function(error,res){
-//       if(error)
-//           return `Nothing in there `;
-      
-//       console.table(res);
+  //
+  //};
+  // function viewAllDepartments(){
 
-//    });
-   
+  //   return connection.query(sqlQuery,function(error,res){
+  //       if(error)
+  //           return `Nothing in there `;
 
-  
+  //       console.table(res);
+
+  //    });
 };
